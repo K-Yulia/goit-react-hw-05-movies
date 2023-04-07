971ad541686a4fab2b0cfc0dd2e94f5056904546
Yulia Kutova <kutova85@gmail.com>
2023-04-07 14:10:13 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import { Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+import { Layout } from './Layout/Layout';
+import { GlobalStyle } from './GlobalStyle';
+
+
+
+const Home = lazy(() => import('../pages/Home'));
+const Movies = lazy(() => import('../pages/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
 export const App = () => {
+  
+ 
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+    <GlobalStyle />
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/:movieId" element={<MovieDetails />} >
+              <Route path="Cast" element={<Cast />} />
+              <Route path="Reviews" element={<Reviews/>} />
+          </Route>
+      </Route>
+      {/* <Route path="*" element={<h2>NotFound</h2>} /> */}
+      </Routes>
+  </>
   );
-};
+}                       
