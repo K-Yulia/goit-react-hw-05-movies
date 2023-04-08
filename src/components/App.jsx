8e858e281850d1cb47245ffea9670e6c7ea908b5
Yulia Kutova <kutova85@gmail.com>
2023-04-09 00:02:22 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,8 +25,7 @@ export const App = () => {
               <Route path="reviews" element={<Reviews/>} />
           </Route>
       </Route>
-      {/* <Route path="*" element={<h2>NotFound</h2>} /> */}
-      </Routes>
+    </Routes>
   </>
   );
 }                       
